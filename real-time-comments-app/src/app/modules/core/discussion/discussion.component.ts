62eb5e7cabeb6b118c1932796de0976ec5962522
Stiliyan <stiliqnskiq.st@abv.bg>
2023-05-24 11:01:35 +0300

    establishing basig connection

--- real-time-comments-app/src/app/modules/core/discussion/discussion.component.ts
+++ real-time-comments-app/src/app/modules/core/discussion/discussion.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SignalRService } from '../../shared/services/signal-r.service';
 
 @Component({
     selector: 'app-discussion',
     templateUrl: './discussion.component.html',
     styleUrls: ['./discussion.component.css']
 })
-export class DiscussionComponent {
-    constructor() { }
+export class DiscussionComponent implements OnInit {
+    constructor(private signalRService: SignalRService) { }
+
+    ngOnInit(): void {
+        this.signalRService.startConnection();
+    }
 }
--- real-time-comments-app/src/app/modules/shared/services/signal-r.service.ts
+++ real-time-comments-app/src/app/modules/shared/services/signal-r.service.ts
@@ -1,7 +1,27 @@
 import { Injectable } from '@angular/core';
 
+import * as SignalR from '@microsoft/signalr';
+
 @Injectable()
 export class SignalRService {
+    private hubConnection!: SignalR.HubConnection;
+
     constructor(
     ) { }
+
+    public startConnection(): Promise<void> {
+        this.hubConnection = new SignalR.HubConnectionBuilder()
+            .withUrl('http://localhost:5000/commentsHub')
+            .withAutomaticReconnect()
+            .build();
+
+        return this.hubConnection
+            .start()
+            .then(() => {
+                console.log('Hub connection successful!');
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
 }
